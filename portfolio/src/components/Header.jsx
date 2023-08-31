4b4225d0a732b8da58b002b1d214e930ba2542be
Timothy <timsu100@gmail.com>
2023-08-30 21:33:43 -0700

    add router to main

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+  return (
+    <div>
+      <h1>Timothy Su's Portfolio</h1>
+    </div>
+  )
+}
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+import App from './App.jsx'
+import Error from './pages/Error'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import Resume from './pages/Resume'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    errorElement:<Error/>, 
+    children: [
+      {
+        index: true,
+        element: <About/>,
+      },
+      {
+        path:'/contact',
+        element:<Contact/>
+      },
+      {
+        path:'/portfolio',
+        element:<Portfolio/>,
+      },
+      {
+        path:'/resume',
+        element:<Resume/>,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
